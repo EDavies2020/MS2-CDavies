f0818c4109cf459e956bf3150c544e742ade4ad4
EDavies2020 <emz201080@yahoo.co.uk>
2020-11-21 23:45:03 +0000

    work on read more/read less javascript to reduce text space in My Journey and My Philosophy columns

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,7 +1,36 @@
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
 }
-
+document.addEventListener("DOMContentLoaded", function(){
+  var menu = document.getElementsByClassName("openmenu");
+    if (menu !== undefined && menu.length == 1){
+    menu[0].addEventListener('click', openNav);
+    /*$(".openmenu").click(openNav);*/
+  }
+}); 
 function closeNav() {
   document.getElementById("myNav").style.height = "0%";
+}
+document.addEventListener("DOMContentLoaded", function(){
+  var menu = document.getElementsByClassName("closemenu");
+    if (menu !== undefined && menu.length == 1){
+    menu[0].addEventListener('click', closeNav);
+    /*$(".openmenu").click(openNav);*/
+  }
+});
+
+function myFunction() {
+  var dots = document.getElementByClassName("dots");
+  var moreText = document.getElementByClassName("more");
+  var btnText = document.getElementByClassName("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more"; 
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less"; 
+    moreText.style.display = "inline";
+  }
 }
