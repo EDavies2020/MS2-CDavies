d94a4fb2eac7615c83aa58a56900cde06262dc9d
EDavies2020 <emz201080@yahoo.co.uk>
2020-11-22 21:47:21 +0000

    added footer, needs to address issues with positioning due too css absolute positioning

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -19,18 +19,3 @@ document.addEventListener("DOMContentLoaded", function(){
   }
 });
 
-function myFunction() {
-  var dots = document.getElementByClassName("dots");
-  var moreText = document.getElementByClassName("more");
-  var btnText = document.getElementByClassName("myBtn");
-
-  if (dots.style.display === "none") {
-    dots.style.display = "inline";
-    btnText.innerHTML = "Read more"; 
-    moreText.style.display = "none";
-  } else {
-    dots.style.display = "none";
-    btnText.innerHTML = "Read less"; 
-    moreText.style.display = "inline";
-  }
-}
