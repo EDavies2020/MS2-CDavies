08cacf6d79078b14d009c78a48c4e29179676bdd
EDavies2020 <emz201080@yahoo.co.uk>
2020-11-25 21:16:20 +0000

    first two boxes added to therapy page and css and modals completed

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -18,23 +18,3 @@ document.addEventListener("DOMContentLoaded", function(){
     /*$(".openmenu").click(openNav);*/
   }
 });
-
-
-
-
-
-
-var modal = document.getElementById("myModal");
-var btn = document.getElementById("myBtn");
-var span = document.getElementsByClassName("close")[0];
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-span.onclick = function() {
-  modal.style.display = "none";
-}
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
