58149a6710532a8b82b2728a6658270fdc5df3a0
EDavies2020 <emz201080@yahoo.co.uk>
2021-03-29 17:11:26 +0000

    changes made to contact form including adding options for therapy choice and submit modal

--- assets/javascript/sendmail.js
+++ assets/javascript/sendmail.js
@@ -2,26 +2,21 @@ function sendMail(contactForm) {
 var templateParams = {
     "name": contactForm.name.value,
     "emailaddress": contactForm.emailaddress.value,
-    "contractnumber": contactForm.contactnumber.value,
-    "therapy": contactForm.therapy.value,
+    "contactnumber": contactForm.contactnumber.value,
+    "therapyoptions": contactForm.therapyoptions.value,
+    "therapychoice": contactForm.therapychoice.value,
     "therapydetails": contactForm.therapydetails.value,
 };
     emailjs.send("CADavies","website enquiry", templateParams)
-        .then(function(response) {
+        .then(function(response) { 
             console.log('SUCCESS!', response.status, response.text);
         }, function(error) {
             console.log('FAILED...', error);
         });
 }
- 
-$('#form').on('submit', function (e) {
-    var showModal = true;
-    $(".form-control").each(function (element) {
-        if ($(this).val() == "") {
-            showModal = false;
-        }
-    });
-    if (showModal) {
-        $('#submitModal').modal('show');
-    }
-});
+$(function(){
+     $('form').on('submit', function () {                   
+          $('#submitModal').modal('show');
+          return false;
+     });
+ });
