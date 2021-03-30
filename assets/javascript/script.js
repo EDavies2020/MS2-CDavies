4ab0e3470e899915a07a74313e74477fe57d4ff3
EDavies2020 <emz201080@yahoo.co.uk>
2021-03-30 11:55:19 +0000

    added back to top button and function on all pages

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -1,3 +1,5 @@
+// curtain navbar - W3Schools tutorial
+
 function openNav() {
   document.getElementById("myNav").style.height = "100%";
 }
@@ -16,4 +18,26 @@ document.addEventListener("DOMContentLoaded", function(){
     menu[0].addEventListener('click', closeNav);
   }
 });
+
+// Back to top on scroll function - W3Schools tutorial
+
+//Get the button
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
 
