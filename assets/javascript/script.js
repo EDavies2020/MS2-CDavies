701698f0d9fa4bcf5e2a6f4dd2dec5f5ee7258aa
EDavies2020 <emz201080@yahoo.co.uk>
2021-04-03 12:10:54 +0000

    therapies link added to homepage and hero image to springboard page

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -50,11 +50,11 @@ function readMore(info) {
 
     if (dots.style.display === "none") {
         dots.style.display = "inline";
-        btnText.textContent = "Read more";
+        btnText.textContent = "Read more...";
         moreText.style.display = "none";
     } else {
         dots.style.display = "none";
-        btnText.textContent = "Read less"; 
+        btnText.textContent = "Read less..."; 
         moreText.style.display = "inline";
     }
 }
