a97beae772acc0dfb6f62606ba7823c11d914ffd
EDavies2020 <emz201080@yahoo.co.uk>
2021-04-06 12:11:38 +0000

    map changed to clustermap and addresses added to ptovide details for the markers

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,11 +1,42 @@
 function initMap() {
-        const dryslwyn = { lat: 51.870022, lng: -4.105435 };
-        const map = new google.maps.Map(document.getElementById("map"), {
-          zoom: 14,
-          center: dryslwyn,
-        });
-        const marker = new google.maps.Marker({
-          position: dryslwyn,
-          map: map,
-        });
-      }
+  const map = new google.maps.Map(document.getElementById("map"), {
+    zoom: 12,
+    center: { lat: 51.870022, lng: -4.105435 },
+  });
+
+  const labels = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  const markers = locations.map((location, i) => {
+    return new google.maps.Marker({
+      position: location,
+      label: labels[i % labels.length],
+    });
+  });
+ 
+  new MarkerClusterer(map, markers, {
+    imagePath:
+      "https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m",
+  });
+}
+const locations = [
+  { lat: 51.869867, lng: -4.105111 },
+  { lat: 51.870190, lng: -4.105309 },
+  
+];
+
+
+
+
+
+
+// function initMap() {
+//         const dryslwyn = { lat: 51.870022, lng: -4.105435 };
+//         const map = new google.maps.Map(document.getElementById("map"), {
+//           zoom: 14,
+//           center: dryslwyn,
+//         });
+//         const marker = new google.maps.Marker({
+//           position: dryslwyn,
+//           map: map,
+//         });
+//       }
